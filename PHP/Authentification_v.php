7a4e1d47a4e7e203b7783ea2d6eeaf2b8083d5b8
alexm <alex.microsoft2003@gmail.com>
2023-05-31 15:53:11 +0200

    TEST

--- PHP/Authentification_v.php
+++ PHP/Authentification_v.php
@@ -9,40 +9,49 @@
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- On importe le fichier style.css -->
-    <link rel="stylesheet" href="CSS/style.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="CSS/style2.css" media="screen" type="text/css" />
 </head>
 <body>
 
 <!-- entête de la page -->
+
 <div id="header">
-
-    <h1><span>Rythmic - image</span></h1>
-
+    <span><img class="ryt" src="Ressources/Logo.png"> </span>
 </div>
 
 <!-- formulaire de connexion -->
-<div id="container">
-    <form action="#" method="POST">
 
-        <!-- titre du formulaire -->
-        <h3 class="rectangle">Connexion j'ai déjà un compte Rythmic</h3>
+<div id="container" class="container">
+    <div class="row justify-content-center">
+        <form action="#" method="POST">
 
+            <!-- titre du formulaire -->
+            <p class="rectangle">Connexion j'ai déjà un compte Rythmic</p>
 
-        <label for="username"><b>Email utilisateur</b></label>
-        <input type="text" placeholder="Entrer l'email de l'utilisateur" name="username" required>
+            <div class="mb-3">
+                <label for="username" class="form-label"><b>Email utilisateur</b></label>
+                <input type="text" class="form-control" id="username" placeholder="Entrer l'email de l'utilisateur" name="username" required>
+            </div>
 
-        <!-- champ de saisie pour le mot de passe -->
-        <label for="password"><b>Mot de passe</b></label>
-        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+            <!-- champ de saisie pour le mot de passe -->
+            <div class="mb-3">
+                <label for="password" class="form-label"><b>Mot de passe</b></label>
+                <input type="password" class="form-control" id="password" placeholder="Entrer le mot de passe" name="password" required>
+            </div>
 
-        <!-- bouton de soumission du formulaire -->
-        <button type="submit" id='submit'>LOGIN</button>
+            <!-- bouton de soumission du formulaire -->
+            <button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
 
-        <!-- lien vers page enregistrement -->
-        <p>vous n'avez pas de compte</p>
-        <a href="#" id="inconnue">inscrivez-vous</a>
+            <!-- lien vers page enregistrement -->
+            <p class="text-center mt-3 mb-0">Vous n'avez pas de compte ? <a href="#" class="d-inline">Inscrivez-vous</a></p>
 
-    </form>
+        </form>
+    </div>
 </div>
+</div>
+
+<!-- On importe les scripts Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pzjFcF1pII3ZbJ+1B+IqOi/veUcibwlqTlYlMLevdvht+GqBnhHhU9i3zMfO2b8v"
+        crossorigin="anonymous"></script>
 </body>
-</html>
