6deb4749cc8879615a633cbd9b60185b9cf985ac
Mathis Meunier <mathis.meunier@isen-ouest.yncrea.fr>
2023-05-31 17:15:51 +0200

    modification age de int à date

--- SQL/model.sql
+++ SQL/model.sql
@@ -62,7 +62,7 @@ CREATE TABLE public.Utilisateur(
 	ID           SERIAL NOT NULL ,
 	Prenom       VARCHAR (70) NOT NULL ,
 	Nom          VARCHAR (50) NOT NULL ,
-	Age          INT  NOT NULL ,
+	Age          DATE  NOT NULL ,
 	Mail         VARCHAR (100) NOT NULL ,
 	Password     VARCHAR (100) NOT NULL ,
 	ID_Morceau   INT    ,
