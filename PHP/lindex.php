1fdb4a7e20e567f923b7a96e3c5d6ff417633382
Allan Cueff <allan.cueff@isen-ouest.yncrea.fr>
2023-06-01 20:58:40 +0200

    update style

--- PHP/lindex.php
+++ PHP/lindex.php
@@ -10,6 +10,7 @@
     <!-- On importe le fichier de style Bootstrap 5 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
     <!-- On importe le fichier de style google fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -34,6 +35,56 @@
 
     <ul class="list-group">
         <li class="list-group-item">File d'attente</li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique avec un nom long</p>
+            <p class="attente_info">Imagine Dragons<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
+        <li class="list-group-item">
+            <img src="../Ressources/album.png">
+            <p class="attente_title">Musique</p>
+            <p class="attente_info">Greg<br>3:31</p>
+        </li>
         <li class="list-group-item">
             <img src="../Ressources/album.png">
             <p class="attente_title">Musique</p>
@@ -62,30 +113,28 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Titres Likes</a>
+                            <button>Titres Likes</button>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Vos Playlists</a>
+                            <button>Vos Playlists</button>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-filter"></i>
-                            </a>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <div class="input-group dropdown">
+                            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn dropdown-toggle helper-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-filter"></i></button>
+                            <button id="search-button" class="btn helper-btn" type="submit"><i class="fas fa-search"></i></button>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
                                 <li><a class="dropdown-item" href="#">Another action</a></li>
                                 <li><hr class="dropdown-divider"></li>
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit"><i class="fas fa-search"></i></button>
+                        </div>
                     </form>
-                    <button class="btn btn-link" type="button"><i class="fas fa-sign-out-alt"></i></button>
+                    <button id="disconnect"><i class="fas fa-sign-out-alt"></i></button>
                 </div>
             </div>
         </nav>
