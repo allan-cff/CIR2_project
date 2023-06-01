ddf72cc7813c2f9f007c0379193d605b2660be4b
alexm <alex.microsoft2003@gmail.com>
2023-06-01 10:54:38 +0200

    TEST

--- PHP/lindex.php
+++ PHP/lindex.php
@@ -8,6 +8,36 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <!-- On importe le fichier de style google fonts -->
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <!-- On importe le fichier style.css -->
     <link rel="stylesheet" href="../CSS/style_index.css" media="screen" type="text/css" />
 </head>
+<body>
+
+<div class="sidebar">
+    <div class="logo">
+        <a href="#">
+            <img src="../Ressources/Logo.png" alt="Logo" />
+        </a>
+    </div>
+    <div class="navigation">
+        <ul>
+            <li>
+                <a href="#">
+                <span class="fa-regular fa-user"></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+<script
+<!--  src="https://kit.fontawesome.com/23cecef777.js"-->
+ <!-- crossorigin="anonymous" pas forcement utile pour le moment on laisse mais si marche pas on retire -->
+></script>
+</body>
+</html>
