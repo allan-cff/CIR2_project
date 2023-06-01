118eef82b879a3c98413eca7b8995b46fa40c172
alexm <alex.microsoft2003@gmail.com>
2023-06-01 14:41:35 +0200

    TEST

--- PHP/lindex.php
+++ PHP/lindex.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Rythmic - Acceuil</title>
+    <title>Rythmic - Accueil</title>
 
     <!-- On importe les icones fontawesome -->
     <script src="https://kit.fontawesome.com/4e06933d63.js" crossorigin="anonymous"></script>
@@ -12,7 +12,6 @@
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- On importe le fichier de style google fonts -->
-
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- On importe le fichier style.css -->
@@ -40,15 +39,14 @@
                 </a>
             </li>
 
-
             <li>
-                <a href="#"> <!-- est mené à être modifier pour que quand on clique, on soit dirigé vers la page de modification de profil -->
+                <a href="#">
                     <span class="rectangle">réglages</span>
                 </a>
             </li>
 
             <li>
-                <a href="#"> <!-- est mené à être modifier pour que l'on puisse voir les prochains titre à venir -->
+                <a href="#">
                     <span class="rectangle">prochains titres</span>
                 </a>
             </li>
@@ -56,10 +54,11 @@
     </div>
 </div>
 
+<div class="main-container">
 
-<script
-<!--  src="https://kit.fontawesome.com/23cecef777.js"-->
- <!-- crossorigin="anonymous"> pas forcement utile pour le moment on laisse mais si marche pas on retire -->
-</script>
+</div>
+
+<!-- <script src="https://kit.fontawesome.com/23cecef777.js" crossorigin="anonymous"></script> -->
+
 </body>
 </html>
